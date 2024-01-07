0670c135b2187f507c15ba530467d5055a268b66
Nicolas L <nicolas62974@gmail.com>
2024-01-07 11:31:18 +0100

    Change version code variable

--- emugui.py
+++ emugui.py
@@ -194,7 +194,7 @@ class Window(QMainWindow, Ui_MainWindow):
         except:
             pass
 
-        self.versionCode = 5512
+        self.versionCode = 5600
 
         if platform.system() == "Windows":
             self.connection = platformSpecific.windowsSpecific.setupWindowsBackend()
