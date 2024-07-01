45f0618971a1018896bc7c6fa8d24169c9f826da
Nicolas <nicolas62974@gmail.com>
2024-07-01 17:17:23 +0200

    Pathfinder no longer utilised on Linux

--- services/pathfinder.py
+++ services/pathfinder.py
@@ -12,12 +12,12 @@ def retrieveExecFolder():
         elif __file__.endswith(".pyc"):
             exec_dir = re.sub(r"_internal\\services\\pathfinder.pyc$", "", __file__)
 
-    else:
+    """ else:
         if __file__.endswith(".py"):
             exec_dir = re.sub("services/pathfinder.py$", "", __file__)
 
         elif __file__.endswith(".pyc"):
-            exec_dir = re.sub("_internal/services/pathfinder.pyc$", "", __file__)
+            exec_dir = re.sub("_internal/services/pathfinder.pyc$", "", __file__) """
 
     return exec_dir
 
